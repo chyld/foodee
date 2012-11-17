3ee2f544d936347ef93882f4043ec1a24e4ce57c
Chyld Medford <chyld.medford@gmail.com>
2012-11-16 23:33:41 -0500

    changes

--- config/initializers/carrierwave_init.rb
+++ config/initializers/carrierwave_init.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                               # required
     :aws_access_key_id      => ENV['AWSKEY'],                       # required
-    :aws_secret_access_key  => ENV['AWSSEC',                        # required
+    :aws_secret_access_key  => ENV['AWSSEC'],                       # required
     :region                 => 'us-east-1'                          # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'foodee'                                  # required
